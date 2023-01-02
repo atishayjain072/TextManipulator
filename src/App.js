039fe525cb6eb372440438b944964956598f47ec
atishayjain072 <jain.atishay072@gmail.com>
2023-01-03 01:51:23 +0530

    Adding React Router

--- src/App.js
+++ src/App.js
@@ -4,35 +4,53 @@ import TextBox from "./components/TextBox";
 import About from "./components/About";
 import Alert from "./components/Alert";
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); //whether dark mode enabled or not
-  const toggleMode=()=>{
-    if (mode==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor='black';
+  const [mode, setMode] = useState("light"); //whether dark mode enabled or not
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark Mode enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode enabled", "success");
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor='white';
-    }
-  }
+  };
   const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
-
-  }
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextBox showAlert={showAlert} heading="Enter the Text to Analyse below" mode={mode} />
-      </div>
-      <About />
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextBox
+                  showAlert={showAlert}
+                  heading="Enter the Text to Analyse below"
+                  mode={mode}
+                />
+              }
+            />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
